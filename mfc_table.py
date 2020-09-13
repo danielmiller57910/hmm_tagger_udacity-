d2ec33c831fc5deaade0817c9740176b0edd3022
Daniel Miller <daniel.miller@sainsburys.co.uk>
2020-09-13 10:52:45 +0100

    mfc lookup table almost complete

---
+++ mfc_table.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+TRAINING_EMISSION_MAX_PATH = os.path.join(os.getcwd(), "training_emission_max.csv")
+
+
+train_df = pd.read_csv(TRAINING_EMISSION_MAX_PATH)
+train_df.drop(columns=["Unnamed: 0"], inplace=True)
+print(train_df.head(50))
+print(train_df.loc[train_df["Word"] == "time"])
+df = train_df.groupby("Count").max()
+print(df.head(50))
+print(df.loc[df["Word"] == "time"])
--- training_data.py
+++ training_data.py
@@ -3,7 +3,15 @@ from collections import namedtuple
 from pair_count import pair_counts
 from helpers import show_model, Dataset
 import pair_count
+import os
 
+TAG_PATH = os.path.join(os.getcwd(), "tags-universal.txt")
+BROWN_PATH  = os.path.join(os.getcwd(), "brown-universal.txt")
+
+TRAINING_ALL_WORD_PATH = os.path.join(os.getcwd(), "training_all_words.csv")
+TRAINING_UNIQUE_WORD_PATH = os.path.join(os.getcwd(), "training_unique_words.csv")
+TRAINING_EMISSION_MAX_PATH = os.path.join(os.getcwd(), "training_emission_max.csv")
+TRAINING_JSON_MAX_EMISSION_PATH = os.path.join(os.getcwd(), "training_out_emission.json")
 
 FakeState = namedtuple("FakeState", "name")
 
@@ -20,11 +28,14 @@ class MFCTagger:
         return 0., list(enumerate(["<start>"] + [self.table[w] for w in seq] + ["<end>"]))
 
 
-data = Dataset(pair_count.TAG_PATH, pair_count.BROWN_PATH, train_test_split=0.8)
+data = Dataset(TAG_PATH, BROWN_PATH, train_test_split=0.8)
+word_counts = pair_counts(
+    data.training_set.vocab,
+    data.training_set,
+    TRAINING_UNIQUE_WORD_PATH,
+    TRAINING_ALL_WORD_PATH,
+    TRAINING_EMISSION_MAX_PATH,
+    TRAINING_JSON_MAX_EMISSION_PATH
+)
 
-print(dir(data.training_set))
-
-for i, j in enumerate(data.training_set.stream()):
-    print(i, j)
-    if i == 10:
-        break
+print(word_counts["NOUN"]["time"])
