988852ccb28bc11a70b2a3efa8c49d6c3ebf7197
Daniel Miller <daniel.miller@sainsburys.co.uk>
2020-09-15 10:04:08 +0100

    training word probability matrix complete: moving onto dictionary

--- step_5.py
+++ step_5.py
@@ -10,8 +10,8 @@ TRAINING_ALL_WORD_PATH = os.path.join(os.getcwd(), "training_all_words.csv")
 
 TAG_PATH = os.path.join(os.getcwd(), "tags-universal.txt")
 BROWN_PATH  = os.path.join(os.getcwd(), "brown-universal.txt")
+TRAINING_WORD_PROBABILITY_MATRIX = os.path.join(os.getcwd(), "training_word_probability_matrix.csv")
 
-# data = Dataset(TAG_PATH, BROWN_PATH, train_test_split=0.8)
 all_word = pd.read_csv(TRAINING_ALL_WORD_PATH, index_col=False)
 all_word.drop(columns=['Unnamed: 0'], inplace=True)
 sample = all_word[all_word['Word'] == 'time'].groupby(['Word', 'Type']).size()
@@ -39,11 +39,9 @@ res.fillna(0, inplace=True)
 
 value_columns = [col for col in res.columns if col not in ['Word', 'SUM']]
 res['SUM'] = res[value_columns].sum(axis=1)
-sample = res[res['Word'] == 'time']
-print(sample.columns)
-print(value_columns)
-for col in value_columns:
-    sample[col] = sample[col] / sample['SUM']
 
-print(sample.head(10))
-# res['SUM'] = df[list(df.columns)].sum(axis=1)
+for col in value_columns:
+    res[col] = res[col] / res['SUM']
+
+res.to_csv(TRAINING_WORD_PROBABILITY_MATRIX)
+
